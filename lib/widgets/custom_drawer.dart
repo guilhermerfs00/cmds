751f15d680e8b1594a534a4c3c7ff03ae4b9ece5
guilhermerfs00 <guilherme.rfs00@gmail.com>
2020-07-04 18:59:03 -0300

    Botão sigout configurado no drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,8 @@
+import 'package:cmds/models/user_model.dart';
+import 'package:cmds/screens/login_screen.dart';
 import 'package:cmds/tile/drawer_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
@@ -17,61 +20,68 @@ class CustomDrawer extends StatelessWidget {
           ),
         );
 
-    return Drawer(
-      child: Stack(
-        children: <Widget>[
-          _buildDrawerBack(),
-          ListView(
-            padding: EdgeInsets.only(left: 32.0, top: 16.0),
+    return ScopedModelDescendant<UserModel>(
+      builder: (context, child, model) {
+        return Drawer(
+          child: Stack(
             children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(bottom: 8.0),
-                padding: EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 8.0),
-                height: 170,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      top: 20.0,
-                      left: 0.0,
-                      child: Text(
-                        'CMDS',
-                        style: TextStyle(fontSize: 34.0),
-                      ),
-                    ),
-                    Positioned(
-                      left: 0.0,
-                      bottom: 0.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Bem vindo !',
-                            style: TextStyle(
-                                fontSize: 18.0, fontWeight: FontWeight.bold),
+              _buildDrawerBack(),
+              ListView(
+                padding: EdgeInsets.only(left: 32.0, top: 16.0),
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 8.0),
+                    padding: EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 8.0),
+                    height: 170,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          top: 20.0,
+                          left: 0.0,
+                          child: Text(
+                            'CMDS',
+                            style: TextStyle(fontSize: 34.0),
                           ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Divider(),
-              DrawerTile(Icons.home, 'Início', () {
-                print('Inicio');
-              }),
-              DrawerTile(Icons.dashboard, 'Dashboard', () {
-                print('DashBoard');
-              }),
-              DrawerTile(Icons.notifications, 'Notificações', () {
-                print('Notificação');
-              }),
-              DrawerTile(Icons.exit_to_app, 'Sair', () {
-                print('Sair');
-              }),
+                        ),
+                        Positioned(
+                          left: 0.0,
+                          bottom: 0.0,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Bem vindo !',
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  DrawerTile(Icons.home, 'Início', () {
+                    print('Inicio');
+                  }),
+                  DrawerTile(Icons.dashboard, 'Dashboard', () {
+                    print('DashBoard');
+                  }),
+                  DrawerTile(Icons.notifications, 'Notificações', () {
+                    print('Notificação');
+                  }),
+                  DrawerTile(Icons.exit_to_app, 'Sair', () {
+                    model.signOut();
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  }),
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
     );
   }
 }
